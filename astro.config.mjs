2c72c2fe828b67e09d3dafee5646bda4174c45b1
Charles Pletcher <pletcher@users.noreply.github.com>
2025-01-08 21:04:41 -0500

    Remove github-specific config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://charlespletcher.org'
-});
+export default defineConfig({});
